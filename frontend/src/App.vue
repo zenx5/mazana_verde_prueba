570fe6711a6c15d884acd195b2e28a38667fd1de
Octavio Martinez <omartinez1618@gmail.com>
2023-08-31 09:18:00 -0400

    pages

--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-</script>
-
-<template>
-  <div class="w-screen h-screen m-0 p-0 bg-gradient-to-br from-slate-400 via-slate-100 to-slate-400">
-    <RouterView />
-  </div>
-</template>
